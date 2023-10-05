0475cdc8abc45f289efbd00ef348b98133484059
Daniel Cukier <danicuki@gmail.com>
2023-10-05 12:01:04 -0300

    first version of blockchain comparison website

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Link from "next/link"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head></Head>
       <body>
         <Main />
         <NextScript />
